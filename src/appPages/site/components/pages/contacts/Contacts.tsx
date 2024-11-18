e50a0d69dfbadca78834336cce1024b28738174e
Abdurakhman <abdibaitovabdurahman@gmail.com>
2024-11-18 11:46:51 +0600

    commit

--- src/appPages/site/components/pages/contacts/Contacts.tsx
+++ src/appPages/site/components/pages/contacts/Contacts.tsx
@@ -1,10 +1,13 @@
+import Feedback from "../about_me/feedback/Feedback";
 import scss from "./Contacts.module.scss";
 
 const Contacts = () => {
   return (
     <div className={scss.contacts}>
       <div className="container">
-        <div className={scss.content}></div>
+        <div className={scss.content}>
+          <Feedback/>
+        </div>
       </div>
     </div>
   );
