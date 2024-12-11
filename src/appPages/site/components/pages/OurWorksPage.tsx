cce765c20e9899eabbc05691640474c2429b3f98
Abdurakhman <abdibaitovabdurahman@gmail.com>
2024-12-11 15:40:03 +0600

    fix some bugs

--- src/appPages/site/components/pages/OurWorksPage.tsx
+++ src/appPages/site/components/pages/OurWorksPage.tsx
@@ -1,10 +1,16 @@
+import OrderSite from "@/ui/orderSite/OrderSite";
 import OurWorks from "./our_works/OurWorks";
 import scss from "./OurWorksPage.module.scss";
+import Feedback from "./about_me/feedback/Feedback";
 const OurWorksPage = () => {
   return (
     <>
       <div className={scss.content}>
         <OurWorks/>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+        <OrderSite/>
+        </div>
+        <Feedback/>
       </div>
     </>
   );
